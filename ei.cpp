b4c1ec1224d63dd5fa4936312fccc2698a987328
wakeupnewbee <32076994+wakeupnewbee@users.noreply.github.com>
2017-10-11 16:51:47 -0400

    Update ei.cpp

--- ei.cpp
+++ ei.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class functor{
+  public:
+    void operator()(int x){
+      cout << x << "this is the number ei!" << endl;
+    }
+};
+
 int main(){
   cout << "hello" << endl;
 }
