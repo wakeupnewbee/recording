3efbde1b6e3e5d0fa8305d53704c3a8e72afe1c0
wakeupnewbee <32076994+wakeupnewbee@users.noreply.github.com>
2017-10-16 14:11:27 -0400

    Update Recursion_solve_array.cpp

--- Recursion_solve_array.cpp
+++ Recursion_solve_array.cpp
@@ -3,13 +3,21 @@ online c++ compiler
 https://www.onlinegdb.com/online_c++_compiler
 */
 
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <iostream>
 
 using namespace std;
 
 int check(int *box, int index){
-    for(int n = 1; n < 10; n++){
-        if(box[index]==box[n]&&index!=n){
+    for(int m = 1; m < 10; m++){
+        if(box[index]==box[m]&&index!=m){
             return 0;
         }
     }
@@ -17,21 +25,32 @@ int check(int *box, int index){
 }
 
 void solver(int *box, int index){
-    for(int n =1; n <10; n++){
-        box[index] = n;
-        if(check(box,index)){
-            solver(box, ++index);
+    if(box[index]==0){
+        for(int n =1; n <10; n++){
+            box[index] = n;
+            if(check(box,index)){
+                index++;
+                return solver(box, index);
+            }
         }
+    }else{
+        index++;
+        return solver(box, index); 
     }
     return;
 }
 
 int main()
 {
-    int box[10]={0};
-    box[9]=2;
+    int box[10]={0,8,0,5,0,9,0,4,0,0};
+    //box[9]=2;
     solver(box,1);
+    cout << "done solver" << endl;
     
+    for(int n=1;n<10;n++){
+        cout << n;
+    }
+    cout << endl;
     for(int n=1;n<10;n++){
         cout << box[n];
     }
