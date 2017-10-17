f5baca1a3671724dcda05c06d3b5f52a7ea555a2
wakeupnewbee <32076994+wakeupnewbee@users.noreply.github.com>
2017-10-17 16:52:49 -0400

    Update sudo_solver_dum_recursion2.cpp

--- sudo_solver_dum_recursion2.cpp
+++ sudo_solver_dum_recursion2.cpp
@@ -9,13 +9,13 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include <iostream>
 using namespace std;
 
-int check(int box[][10], int x, int y){
+int check(int box[][4], int x, int y){
     for(int idx=1; idx<10;idx++){
         if(box[x][y]==box[idx][y]&& idx!=x){
             return 0;
         }
     }
-    for(int idy=1;idy<10;idy++){
+    for(int idy=1;idy<4;idy++){
         if(box[x][y]==box[x][idy]&& idy!=y){
             return 0;
         }
@@ -24,9 +24,9 @@ int check(int box[][10], int x, int y){
 }
 
 
-void f_s(int box[][10],int x,int y,int* left){
-    int show_x,show_y,show_box;
-    if(y==10){
+void f_s(int box[][4],int x,int y,int* left){
+    int show_x,show_y,show_box,show_left;
+    if(y==4){
         return;
     }
     if(box[x][y]==0){
@@ -34,22 +34,30 @@ void f_s(int box[][10],int x,int y,int* left){
             box[x][y]=n;
             show_x=x;
             show_y=y;
+            show_left=*left;
             show_box=box[x][y];
             if(check(box,x,y)){
                 *left = *left - 1;
                 cout << *left << " ";
                 if(x<=8){
                     f_s(box,++x,y,left);
-                    if(left==0) return;
+                    show_left=*left;
+                    if(show_left==0){
+                        return;
+                    }
                     //*left = *left + 1;
                 }else{
                     f_s(box,1,++y,left);//process to the next
-                    if(left==0) return;
+                    show_left=*left;
+                    if(show_left==0){
+                        return;    
+                    } 
                     //*left = *left + 1;
                 }
             }
         }
         *left = *left + 1;
+        show_left=*left;
         box[x][y]=0;
         return;
     }else{
@@ -65,13 +73,13 @@ void f_s(int box[][10],int x,int y,int* left){
 
 int main()
 {
-    int box[10][10]={0};
+    int box[10][4]={0};
 
-    int left = 81;
+    int left = 26;
     f_s(box,1,1,&left);
     cout << endl;
     
-    for(int m=1;m<10;m++){
+    for(int m=1;m<4;m++){
         for(int n=1;n<10;n++){
             cout << box[n][m] <<" ";
         }
